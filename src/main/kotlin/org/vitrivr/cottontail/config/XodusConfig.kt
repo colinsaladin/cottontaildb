ea07a28800dfdbda35e28231a093884fb6081d42
Ralph Gasser <rg@pontius.ch>
2021-09-21 19:59:43 +0200

    Adjusted some configurations.

--- src/main/kotlin/org/vitrivr/cottontail/config/XodusConfig.kt
+++ src/main/kotlin/org/vitrivr/cottontail/config/XodusConfig.kt
@@ -6,12 +6,12 @@ import org.vitrivr.cottontail.storage.io.SharedFileChannelCache
 
 @Serializable
 data class XodusConfig (
-    val logFileSize: Long = 64 * 1024L, /** Determines size of an individual Log file on disk. @see [EnvironmentConfig.LOG_FILE_SIZE] */
-    val logCachePageSize: Int = 64 * 1024, /** Determines size of an individual Log page in cache. @see [EnvironmentConfig.LOG_CACHE_PAGE_SIZE] */
+    val logFileSize: Long = 128 * 1024L, /** Determines size of an individual Log file on disk. @see [EnvironmentConfig.LOG_FILE_SIZE] */
+    val logCachePageSize: Int = 128 * 1024, /** Determines size of an individual Log page in cache. @see [EnvironmentConfig.LOG_CACHE_PAGE_SIZE] */
     val logCacheOpenFilesSize: Int = 500, /** Determines size of the open files cache. @see [EnvironmentConfig.LOG_CACHE_OPEN_FILES] */
     val logCacheReadAhead: Int = 5, /** Determines if and how many pages should be pre-fetched. @see [EnvironmentConfig.LOG_CACHE_READ_AHEAD_MULTIPLE] */
     val storeGetCacheSize: Int = 100, /** Determines size of internal store-get cache. @see [EnvironmentConfig.ENV_STOREGET_CACHE_SIZE] */
-    val treeDupMaximumPageSize: Int = 128, /** Determines maximum size an duplicate B+-tree page. @see [EnvironmentConfig.TREE_DUP_MAX_PAGE_SIZE] */
+    val treeDupMaximumPageSize: Int = 128, /** Determines maximum size of a duplicate B+-tree page. @see [EnvironmentConfig.TREE_DUP_MAX_PAGE_SIZE] */
     val treeMaximumPageSize: Int = 1024, /** Determines the maximum size an individual B+-tree page. @see [EnvironmentConfig.TREE_MAX_PAGE_SIZE] */
     val useCustomIO: Boolean = true /** Determine whether custom data writer/reader class should be used. Defaults to true*/
 ) {
