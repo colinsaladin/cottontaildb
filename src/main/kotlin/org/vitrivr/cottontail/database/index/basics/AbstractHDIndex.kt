97555661fe3df8e6007764f34609abdba7fd20b0
Ralph Gasser <rg@pontius.ch>
2022-01-14 17:21:00 +0100

    Added first, rough implementation of an AbstractHDIndex using an AuxiliaryValueCollection.

--- src/main/kotlin/org/vitrivr/cottontail/database/index/basics/AbstractHDIndex.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/index/basics/AbstractHDIndex.kt
@@ -1,14 +1,18 @@
 package org.vitrivr.cottontail.database.index.basics
 
+import org.vitrivr.cottontail.database.column.ColumnDef
 import org.vitrivr.cottontail.database.entity.DefaultEntity
 import org.vitrivr.cottontail.database.index.Index
-import org.vitrivr.cottontail.database.index.IndexTx
+import org.vitrivr.cottontail.database.index.basics.avc.AuxiliaryValueCollection
+import org.vitrivr.cottontail.database.operations.Operation
 import org.vitrivr.cottontail.execution.TransactionContext
 import org.vitrivr.cottontail.model.basics.Name
+import org.vitrivr.cottontail.model.values.types.VectorValue
+import kotlin.concurrent.withLock
 
 
 /**
- * An abstract [Index] implementation that outlines the fundamental structure of a high-dimensional (HD).
+ * An abstract [Index] implementation that outlines the fundamental structure of a high-dimensional (HD) index.
  *
  * Implementations of [Index]es in Cottontail DB should inherit from this class.
  *
@@ -19,17 +23,72 @@ import org.vitrivr.cottontail.model.basics.Name
  */
 abstract class AbstractHDIndex(name: Name.IndexName, parent: DefaultEntity) : AbstractIndex(name, parent) {
 
+    init {
+        require(this.columns.size == 1) { "High-dimensional indexes do not support indexing of more than a single column." }
+        require(this.columns[0].type.vector) { "High-dimensional indexes only support indexing of vector columns."}
+    }
+
+    /** The [ColumnDef] that is being indexed by this [AbstractHDIndex]. */
+    val column: ColumnDef<*>
+        get() = this.columns[0]
+
     /** The dimensionality of this [AbstractHDIndex]. */
     val dimension: Int
-        get() = this.columns[0].type.logicalSize
+        get() = this.column.type.logicalSize
 
     /**
      * A [Tx] that affects this [AbstractIndex].
      */
-    protected abstract inner class Tx(context: TransactionContext) : AbstractIndex.Tx(context), IndexTx {
+    protected abstract inner class Tx(context: TransactionContext) : AbstractIndex.Tx(context) {
 
-        /** The dimensionality of the [AbstractHDIndex] this [Tx] belongs to. */
-        val dimension: Int
-            get() = super.columns[0].type.logicalSize
+        /** The [AuxiliaryValueCollection] used by this [AbstractHDIndex.Tx]. */
+        protected abstract val auxiliary: AuxiliaryValueCollection
+
+        /**
+         * Tries to process an incoming [Operation.DataManagementOperation]. Such [Operation.DataManagementOperation] can
+         * either be appended to the underlying index (if the write-model allows for it) or appended to the auxiliary value list.
+         *
+         * @param event [Operation.DataManagementOperation]
+         */
+        final override fun update(event: Operation.DataManagementOperation) = this.txLatch.withLock {
+            /* If index does not support incremental updating at all. */
+            if (!this@AbstractHDIndex.supportsIncrementalUpdate) {
+                this.updateState(IndexState.STALE)
+                return@withLock
+            }
+
+            /* If write-model does not allow propagation, apply change to auxiliary value collection. */
+            if (!this.tryApplyToIndex(event)) {
+                when (event) {
+                    is Operation.DataManagementOperation.DeleteOperation -> this.auxiliary.applyDelete(event.tupleId)
+                    is Operation.DataManagementOperation.InsertOperation -> {
+                        val value = event.inserts[this@AbstractHDIndex.column]
+                        if (value is VectorValue<*>) this.auxiliary.applyInsert(event.tupleId)
+                    }
+                    is Operation.DataManagementOperation.UpdateOperation -> {
+                        val value = event.updates[this@AbstractHDIndex.column]?.second
+                        if (value is VectorValue<*>) {
+                            this.auxiliary.applyUpdate(event.tupleId)
+                        } else if (value == null) {
+                            this.auxiliary.applyDelete(event.tupleId)
+                        }
+                    }
+                }
+                this.updateState(IndexState.DIRTY)
+                return@withLock
+            }
+        }
+
+        /**
+         * Tries to apply the change applied by this [Operation.DataManagementOperation] to this [AbstractHDIndex]. This is the
+         * implementation of this [AbstractHDIndex]'s write model.
+         *
+         * This method returns true if, and only if, a change can be applied to an index without impairing its ability to produce correct results.
+         * In such a case the change is applied to the underlying data structure. In all other cases, no changes are applied and the method returns false.
+         *
+         * @param event The [Operation.DataManagementOperation] to apply.
+         * @return True if change could be applied, false otherwise.
+         */
+        protected abstract fun tryApplyToIndex(event: Operation.DataManagementOperation): Boolean
     }
 }
